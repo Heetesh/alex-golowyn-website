2e7a70de76bf846e75f26479c26a59c12c93488a
Heetesh Doorbiz <yudishdz@gmail.com>
2025-09-30 17:14:29 +0800

    tailwindcss + volt primevue added

--- webapp/src/views/AboutView.vue
+++ webapp/src/views/AboutView.vue
@@ -1,16 +1,12 @@
 <template>
-	<div class="about flex flex-col justify-center items-center">
-		<h1 class="text-red-500">This is an about page</h1>
-		<h1>This is an about page</h1>
-	</div>
+	<div></div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-	.about {
-		min-height: 100vh;
-		display: flex;
-		align-items: center;
-	}
+<script lang="ts">
+export default {
+	data() {
+		return {}
+	},
+	methods: {},
 }
-</style>
+</script>

70bf97eacf8ceec90bdd318ebdd36b57ef12b002
Heetesh Doorbiz <yudishdz@gmail.com>
2025-11-11 07:46:02 +0800

    wip: NavigationMenu for mobile MVP done

--- nuxt-app/app/components/AppNavigationMenu.vue
+++ nuxt-app/app/components/AppNavigationMenu.vue
@@ -1,16 +1,36 @@
 <template>
     <div v-if="isDesktop">
-        <UNavigationMenu :items="navigationItems"></UNavigationMenu>
+        <UNavigationMenu :items="navigationItems" />
     </div>
 
-    <div v-else>
-        <p>Implement nav for mobile</p>
+    <div v-else class="flex justify-end bg-neutral-50 rounded-none">
+        <UButton icon="i-lucide-menu" size="xl" variant="ghost" color="primary" @click="openDrawer" />
+
+        <UDrawer v-model:open="isDrawerOpen" direction="right" :ui="{}">
+            <template #body>
+                <div class="flex flex-col min-w-full gap-4">
+                    <NuxtLink v-for="items in navigationItems" v-slot="{ isActive, navigate }" :key="items.label"
+                        :to="items.to" custom>
+                        <UButton class="text-center font-medium transition-colors py-4 justify-center"
+                            :variant="isActive ? 'solid' : 'subtle'" :color="isActive ? 'primary' : 'neutral'"
+                            @click="closeDrawer(); navigate();">{{
+                                items.label
+                            }}</UButton>
+                    </NuxtLink>
+                </div>
+            </template>
+        </UDrawer>
     </div>
+
+    <!-- <UHeader mode="slideover">
+        
+    </UHeader> -->
 </template>
 
 <script lang="ts">
 
 import type { NavigationMenuItem } from '@nuxt/ui';
+import type { NavigationFailure } from 'vue-router';
 
 export default {
     data() {
@@ -58,7 +78,25 @@ export default {
                 },
             ] as NavigationMenuItem[],
             isDesktop: useMediaQuery("(min-width: 825px)"),
+
+            isDrawerOpen: false,
         }
+    },
+
+    methods: {
+        closeDrawer() {
+            if (this.isDrawerOpen) {
+                this.isDrawerOpen = false
+            }
+        },
+
+        openDrawer() {
+            if (!this.isDrawerOpen) {
+                this.isDrawerOpen = true
+            }
+        },
+
+
     }
 }
 </script>

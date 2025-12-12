e376898e8995fad737235f47404dc269888cd805
Heetesh Doorbiz <yudishdz@gmail.com>
2025-12-12 21:36:58 +0800

    updated header, removed nav icons, updated website image, minor nav bar fix

--- nuxt-app/app/components/Navigation/Menu.vue
+++ nuxt-app/app/components/Navigation/Menu.vue
@@ -8,7 +8,6 @@
 		<UNavigationMenu
 			:items="navigationMenuItems"
 			content-orientation="vertical"
-			:highlight="true"
 			color="primary"
 		/>
 		<!-- template body passes passes the UNavigationMenu to the drawer -->
@@ -24,17 +23,15 @@
 <script setup lang="ts">
 import type { NavigationMenuChildItem, NavigationMenuItem } from "@nuxt/ui";
 
-const navigationMenuItems: NavigationMenuItem[] = [
+const navigationMenuItems = computed<NavigationMenuItem[]>(() => [
 	{
 		label: "Home",
-		icon: "material-symbols:home",
 		to: "/",
 	},
 	{
 		label: "Services",
-		icon: "material-symbols:home-repair-service-rounded",
 		to: "/services",
-		exact: false,
+		active: useRoute().path.startsWith("/services"),
 		children: [
 			{
 				label: "Alpha",
@@ -58,41 +55,95 @@ const navigationMenuItems: NavigationMenuItem[] = [
 	},
 	{
 		label: "Weekly Quotes",
-		icon: "ion:ios-quote",
 		to: "/weekly_quotes",
 	},
 	{
 		label: "Weekly Philosophy",
-		icon: "ion:ios-quote",
 		to: "/weekly_philosophy",
 	},
 	{
 		label: "Videos",
-		icon: "material-symbols:hangout-video-rounded",
 		to: "/videos",
 	},
 
 	{
 		label: "Reviews",
-		icon: "material-symbols:rate-review-rounded",
 		to: "/reviews",
 	},
 	{
 		label: "Contact",
-		icon: "material-symbols:contact-phone-rounded",
 		to: "/contact",
 	},
 	{
 		label: "Donations",
-		icon: "solar:dollar-minimalistic-bold",
 		to: "/donations",
 	},
 	{
 		label: "About",
-		icon: "ix:about-filled",
 		to: "/about",
 	},
-];
+]);
+
+// const navigationMenuItems: NavigationMenuItem[] = [
+// 	{
+// 		label: "Home",
+// 		to: "/",
+// 	},
+// 	{
+// 		label: "Services",
+// 		to: "/services",
+// 		active: useRoute().path.startsWith("/services"),
+// 		children: [
+// 			{
+// 				label: "Alpha",
+// 				to: "/services/alpha",
+// 			},
+// 			{
+// 				label: "Beta",
+// 				to: "/services/beta",
+// 			},
+// 			{
+// 				label: "Charlie",
+// 				to: "/services/charlie",
+// 			},
+// 			{
+// 				label: "Delta",
+// 				to: "/services/delta",
+// 			},
+
+// 		] as NavigationMenuChildItem[],
+// 		// active: useRoute().path.startsWith("/services"),
+// 	},
+// 	{
+// 		label: "Weekly Quotes",
+// 		to: "/weekly_quotes",
+// 	},
+// 	{
+// 		label: "Weekly Philosophy",
+// 		to: "/weekly_philosophy",
+// 	},
+// 	{
+// 		label: "Videos",
+// 		to: "/videos",
+// 	},
+
+// 	{
+// 		label: "Reviews",
+// 		to: "/reviews",
+// 	},
+// 	{
+// 		label: "Contact",
+// 		to: "/contact",
+// 	},
+// 	{
+// 		label: "Donations",
+// 		to: "/donations",
+// 	},
+// 	{
+// 		label: "About",
+// 		to: "/about",
+// 	},
+// ];
 </script>
 
 <style scoped></style>
--- nuxt-app/app/layouts/default.vue
+++ nuxt-app/app/layouts/default.vue
@@ -2,21 +2,32 @@
 	<UApp>
 		<NuxtRouteAnnouncer />
 
-		<SpeedInsights />
-
 		<!-- Wraps a UHeader  -->
 		<NavigationMenu />
 
 		<UMain class="relative overflow-hidden">
-			<img
+			<!-- <img
 				:src="heroImage"
 				class="absolute top-0 left-0 right-0 w-full h-[30vh]  object-cover -z-10 opacity-80 mask-[linear-gradient(to_bottom,black_60%,transparent_100%)]"
 				alt="Home page image or hero image."
 				loading="eager"
 				fetchpriority="high"
-			>
+			> -->
 
-			<UContainer class="relative z-10 pt-[15vh]">
+			<UContainer class="pt-4 grayscale-25 flex items-center justify-center">
+				<img
+					:src="heroImage"
+					class="rounded-2xl object-contain"
+					alt="Home page image or hero image."
+					loading="eager"
+					fetchpriority="high"
+				>
+			</UContainer>
+
+			<!-- <UContainer class="relative z-10 pt-[15vh]">
+				<slot />
+			</UContainer> -->
+			<UContainer class="relative z-2">
 				<slot />
 			</UContainer>
 		</UMain>
@@ -43,7 +54,6 @@
 </template>
 
 <script lang="ts" setup>
-import { SpeedInsights } from "@vercel/speed-insights/nuxt";
 import heroImage from "@/assets/images/cliff_sunset_with_beach.jpg";
 </script>
 

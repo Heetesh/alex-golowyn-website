7cc819e9164dd0d69f22577394957efc9d93a6a9
Heetesh Doorbiz <yudishdz@gmail.com>
2025-12-13 13:33:17 +0800

    wip: missing weekly_quote and weekly_philosphy git add

--- nuxt-app/app/pages/weekly_philosophy/index.vue
+++ nuxt-app/app/pages/weekly_philosophy/index.vue
@@ -1,6 +1,108 @@
 <template>
 	<div>
-		<UButton>Weekly Philosophy</UButton>
+		<UPageSection
+			title="Weekly Philosphy"
+			orientation="vertical"
+		>
+			<div class="relative grid grid-cols-1 lg:grid-cols-2 gap-8 justify-between">
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+			</div>
+		</UPageSection>
 	</div>
 </template>
 
--- nuxt-app/app/pages/weekly_quotes/index.vue
+++ nuxt-app/app/pages/weekly_quotes/index.vue
@@ -1,7 +1,34 @@
 <template>
 	<div>
-		<UPageSection>
+		<UPageSection
+			title="Weekly Quotes"
+			orientation="vertical"
+		>
 			<div class="relative grid grid-cols-1 lg:grid-cols-2 gap-8 justify-between">
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
+				<UPageCard
+					highlight
+					variant="subtle"
+					spotlight
+					spotlight-color="primary"
+					description="The pen is mightier than the sword."
+					class="font-semibold"
+				>
+					<template #footer>
+						<UUser name="Edward Bulwer-Lytton" />
+					</template>
+				</UPageCard>
 				<UPageCard
 					highlight
 					variant="subtle"
